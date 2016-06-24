de7986d1ace2e1d628d3c5860cde78aad3353182
diego <diego@localhost.localdomain>
2016-06-24 06:32:46 -0400

    Codigo de seleccion de peliculas terminado

--- prediction.py
+++ prediction.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.io import mmread
 import sklearn.linear_model as lm
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 def list_duplicates_of(seq,item):
@@ -23,25 +24,21 @@ def list_duplicates_of(seq,item):
 
 
 
-data_dir = "./ngrams-deprels-origin.runtime.budget.numscreen.ratings.seasons/"
+#data_dir = "./ngrams-deprels-origin.runtime.budget.numscreen.ratings.seasons/"
+data_dir = "./ngrams-deprels-fp1-origin.runtime.budget.numscreen.ratings.seasons.stars/"
 
 print "importando X..."
 X = csr_matrix( mmread(data_dir+'train.x.mm'))
 print "importando y..."
 y = np.loadtxt(data_dir+'train.y.dat')
 
-'''
-model = lm.LinearRegression(fit_intercept = True)
-model.fit (X,y)
-print "R2=%f"%model.score(X, y)
-'''
 
+# Training
 models = [ ]
 alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
 for i in range(0,10):
     print i
-    models.append( lm.Lasso(alpha=alphas[i], max_iter=1000, tol=0.001) )
+    models.append( lm.Ridge(alpha=alphas[i], max_iter=1000, tol=0.001) )
     models[i].fit (X,y)
     print "R2=%f"%models[i].score(X, y)
 
@@ -53,21 +50,44 @@ print "importando y_val..."
 y_validation = np.loadtxt(data_dir+'dev.y.dat')
 
 
-#testing
 
-# The mean square error
+# Validación
 MSEs = [ ]
 for i in range(0,10):
-    MSEs.append = np.mean((models[i].predict(X_validation) - y_validation) ** 2)
-    print("Residual sum of squares Nº" + str(i) + "{0:.2f}".format(MSEs[i]) )
+    MSEs.append( np.mean((models[i].predict(X_validation) - y_validation) ** 2) )
+    print("Residual sum of squares Nº" + str(i) + ": {0:.2f}".format(MSEs[i]) )
 
 print list_duplicates_of(MSEs, min(MSEs))
 selected_index = MSEs.index(min(MSEs))
 print MSEs.index(min(MSEs))
 
+
+
+# Testing
 print "importando X_test..."
 X_test = csr_matrix( mmread(data_dir+'test.x.mm'))
 print "importando y_test..."
 y_test = np.loadtxt(data_dir+'test.y.dat')
 
 print "R2=%f"%models[selected_index].score(X_test, y_test)
+
+
+
+# Plotting
+
+model = lm.Ridge()
+contador=0
+x_list=[]
+y_list=[]
+while contador<3000:
+    model.set_params(alpha=contador, max_iter=3000)
+    model.fit(X, y)
+    print "R2=%f" % model.score(X_test, y_test)
+    x_list.append(contador)
+    y_list.append(model.score(X_test, y_test))
+
+    contador += 50
+
+
+plt.plot(x_list, y_list, label='R2 en funcion de alpha')
+plt.show()
